13449ecfc8229f5c9c12cfcc53c361aabcae4b7b
Denim Mazuki <denimmazuki@gmail.com>
2017-03-11 17:04:49 -0800

    Created test for InputViewController and testing it

---
+++ ToDoApp/InputViewController.swift
@@ -0,0 +1,14 @@
+//
+//  InputViewController.swift
+//  ToDoApp
+//
+//  Created by Denim Mazuki on 3/11/17.
+//  Copyright © 2017 Denim Mazuki. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    @IBOutlet var titleTextField: UITextField!
+    
+}
---
+++ ToDoAppTests/InputViewControllerTests.swift
@@ -0,0 +1,32 @@
+//
+//  InputViewControllerTests.swift
+//  ToDoApp
+//
+//  Created by Denim Mazuki on 3/11/17.
+//  Copyright © 2017 Denim Mazuki. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoApp
+
+class InputViewControllerTests: XCTestCase {
+    
+    var sut: InputViewController!
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: "InputViewController") as! InputViewController
+        _ = sut.view
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func test_HasTitleTextField() {
+        XCTAssertNotNil(sut.titleTextField)
+    }
+    
+}
