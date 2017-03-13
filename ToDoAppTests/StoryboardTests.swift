fbc97d3a0132373ead0d9603e61750da9e3e7368
Denim Mazuki <denimmazuki@gmail.com>
2017-03-13 14:17:40 -0700

    Added test for Storyboard and tested if ListViewController is initial view controller

---
+++ ToDoAppTests/StoryboardTests.swift
@@ -0,0 +1,33 @@
+//
+//  StoryboardTests.swift
+//  ToDoApp
+//
+//  Created by Denim Mazuki on 3/13/17.
+//  Copyright © 2017 Denim Mazuki. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoApp
+
+class StoryboardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_InitialViewController_IsItemListViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        let rootViewController = navigationController.viewControllers[0]
+        
+        XCTAssertTrue(rootViewController is ItemListViewController)
+    }
+    
+}
