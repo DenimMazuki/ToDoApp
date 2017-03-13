6f016919f6c34a01131f02967bfb2827200125d7
Denim Mazuki <denimmazuki@gmail.com>
2017-03-13 13:59:39 -0700

    Refactored login test to catch error if data passed in is empty/invalid

--- ToDoAppTests/APIClientTests.swift
+++ ToDoAppTests/APIClientTests.swift
@@ -80,6 +80,27 @@ class APIClientTests: XCTestCase {
         
     }
     
+    func test_Login_WhenJSONIsInvalid_ReturnsError() {
+        
+        let sut = APIClient()
+        let mockURLSession = MockURLSession(data: Data(), urlResponse: nil, error: nil)
+        sut.session = mockURLSession
+        
+        let errorExpectation = expectation(description: "Error")
+        var catchedError: Error? = nil
+        sut.loginUser(withName: "Foo", password: "Bar") {
+            (token, error) in
+            catchedError = error
+            errorExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1) {
+            (error) in
+            XCTAssertNotNil(catchedError)
+        }
+        
+    }
+    
 }
 
 extension APIClientTests {
